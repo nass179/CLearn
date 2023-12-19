458724a09d07c1279af686c2a17c3e15b958d55f
nass179 <nass.shan@googlemail.com>
2023-12-19 13:57:58 +0100

    d

--- leetcode/intSum.c
+++ leetcode/intSum.c
@@ -1,17 +1,27 @@
 #include <stdio.h>
-int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+#include <stdlib.h>
+int* twoSum(const int* nums, int numsSize, int target, int* returnSize) {
+    int *returnArray = malloc(2 * sizeof(int));
+    returnArray[0] = 1;
+    returnArray[1] = 1;
     for (int i = 0; i < numsSize; ++i) {
-        if(nums[i] + nums[i+1] == target){
-            returnSize[0] = nums[i];
-            returnSize[1] = nums[i+1];
-            return returnSize;
+        for (int j = i+1; j < numsSize; ++j) {
+            if(nums[i] + nums[j] == target){
+                *returnSize = 2;
+                returnArray[0] = i;
+                returnArray[1] = j;
+                return returnArray;
+            }
         }
     }
+    returnSize = 0;
+    return NULL;
 }
 int main(){
     int nums[] = {1, 2, 4, 6};
     int target = 3;
     int numSize = 4;
-    int returnSize[] = {0, 0};
-    printf("%d",twoSum(nums, numSize, target, returnSize)[0]);
+    int a = 2;
+    int *returnSize = &a;
+    printf("[%d,%d]",twoSum(nums, numSize, target, returnSize)[0],  twoSum(nums, numSize, target, returnSize)[1]);
 }
