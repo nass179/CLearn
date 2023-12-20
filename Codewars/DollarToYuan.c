b3c3e2f28eb8a18d30bd0a7ef94676df52dbdeb0
nass179 <nass.shan@googlemail.com>
2023-12-20 22:13:25 +0100

    e

--- Codewars/DollarToYuan.c
+++ Codewars/DollarToYuan.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
-#include <ctype.h>
 #include <stdlib.h>
-
 /*
  Create a function that converts US dollars (USD) to Chinese Yuan (CNY) .
  The input is the amount of USD as an integer, and the output should be a string that states the amount of Yuan followed by 'Chinese Yuan'
@@ -13,13 +11,12 @@ The conversion rate you should use is 6.75 CNY for every 1 USD.
  All numbers should be represented as a string with 2 decimal places. (e.g. "21.00" NOT "21.0" or "21")
  */
 char *USD_to_CNY (long dollars, char *yuans){
-    double y = dollars * 6.75;
-
-    *yuans = 's'; // write to yuans
+    sprintf(yuans, "%.2Lf Chinese Yuan", (long double) dollars * 6.75);//merken um strings in char pointer abzuspeichern
     return yuans;
 }
 int main(){
-    long dollars = 1;
-    char *yuans= malloc(12 * sizeof(char));
+    long dollars = 10;
+    char *yuans = malloc(sizeof(char));
+
     printf("%s", USD_to_CNY(dollars, yuans));
 }
