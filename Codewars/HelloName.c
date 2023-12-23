579aa4ef3484d430db8dd6a7cec6601086363887
nass179 <nass.shan@googlemail.com>
2023-12-23 18:09:59 +0100

    f

--- Codewars/HelloName.c
+++ Codewars/HelloName.c
@@ -4,27 +4,29 @@
 #include <ctype.h>
 
 char *hello(const char *name) {
-    if(!isalpha(name[0])) return "Hello, World!";
     int len =strlen(name);
     if((name != NULL) && (name[0] == '\0')) return "Hello, World!";
     for (int i = 0; i < len; ++i) {
         if(!isalpha(name[i])) return "Hello, World!";
     }
-
-    char *n = malloc(sizeof(name));
-    sprintf(n, "%s", name);
-    n[0] <<= 3;
-    n[0] >>= 3;
-    n[0] |= 0b01000000;
-    for (int i = 1; i < len; ++i) {
-            n[i] = name[i]|0b01100000;
+    for (int i = 0; i < len; ++i) {
+        if (isalpha(name[i])) {
+            char *n = malloc(sizeof(name));
+            sprintf(n, "%s", name);
+            n[0] <<= 3;
+            n[0] >>= 3;
+            n[0] |= 0b01000000;
+            for (int i = 1; i < len; ++i) {
+                n[i] = name[i] | 0b01100000;
+            }
+            char *hello = malloc(12 * sizeof(char));
+            sprintf(hello, "Hello, %s!", n);//  <----  hajime!
+            return hello;
+        }
     }
-    char *hello = malloc(sizeof(char));
-    sprintf(hello, "Hello, %s!", n);//  <----  hajime!
-    return hello;
 }
 int main(){
-    char *name = "jOHN";
-    free(name);
+    char *name;
+    name = "jHON";
     printf("%s", hello(name));
 }
