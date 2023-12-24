d96980108ced5ab2394252cbc7302450e2e1bfab
nass179 <nass.shan@googlemail.com>
2023-12-24 11:23:44 +0100

    a

---
+++ Codewars/cmake-build-debug/CMakeFiles/Codewars.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Codewars.dir/SameCaseCheck.c.obj"
+  "CMakeFiles/Codewars.dir/SameCaseCheck.c.obj.d"
+  "CMakeFiles/Codewars.dir/main.c.obj"
+  "CMakeFiles/Codewars.dir/main.c.obj.d"
+  "Codewars.exe"
+  "Codewars.exe.manifest"
+  "Codewars.pdb"
+  "libCodewars.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Codewars.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
