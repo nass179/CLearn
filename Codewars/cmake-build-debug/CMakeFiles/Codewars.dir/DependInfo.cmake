1272e22b0859113c11e4ff1c3cb7fb411ade8ae8
nass179 <nass.shan@googlemail.com>
2024-04-11 14:12:38 +0200

    d

--- Codewars/cmake-build-debug/CMakeFiles/Codewars.dir/DependInfo.cmake
+++ Codewars/cmake-build-debug/CMakeFiles/Codewars.dir/DependInfo.cmake
@@ -8,13 +8,13 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "C:/Users/henry/Desktop/CLearn/Codewars/NumOfStud.c" "CMakeFiles/Codewars.dir/NumOfStud.c.obj" "gcc" "CMakeFiles/Codewars.dir/NumOfStud.c.obj.d"
-  "C:/Users/henry/Desktop/CLearn/Codewars/SameCaseCheck.c" "CMakeFiles/Codewars.dir/SameCaseCheck.c.obj" "gcc" "CMakeFiles/Codewars.dir/SameCaseCheck.c.obj.d"
-  "C:/Users/henry/Desktop/CLearn/Codewars/genRaangeofInt.c" "CMakeFiles/Codewars.dir/genRaangeofInt.c.obj" "gcc" "CMakeFiles/Codewars.dir/genRaangeofInt.c.obj.d"
-  "C:/Users/henry/Desktop/CLearn/Codewars/isThisaTriangle.c" "CMakeFiles/Codewars.dir/isThisaTriangle.c.obj" "gcc" "CMakeFiles/Codewars.dir/isThisaTriangle.c.obj.d"
-  "C:/Users/henry/Desktop/CLearn/Codewars/main.c" "CMakeFiles/Codewars.dir/main.c.obj" "gcc" "CMakeFiles/Codewars.dir/main.c.obj.d"
-  "C:/Users/henry/Desktop/CLearn/Codewars/newtester.c" "CMakeFiles/Codewars.dir/newtester.c.obj" "gcc" "CMakeFiles/Codewars.dir/newtester.c.obj.d"
-  "C:/Users/henry/Desktop/CLearn/Codewars/removeExclamation.c" "CMakeFiles/Codewars.dir/removeExclamation.c.obj" "gcc" "CMakeFiles/Codewars.dir/removeExclamation.c.obj.d"
+  "C:/Users/shann/Desktop/C/CLearn/Codewars/NumOfStud.c" "CMakeFiles/Codewars.dir/NumOfStud.c.obj" "gcc" "CMakeFiles/Codewars.dir/NumOfStud.c.obj.d"
+  "C:/Users/shann/Desktop/C/CLearn/Codewars/SameCaseCheck.c" "CMakeFiles/Codewars.dir/SameCaseCheck.c.obj" "gcc" "CMakeFiles/Codewars.dir/SameCaseCheck.c.obj.d"
+  "C:/Users/shann/Desktop/C/CLearn/Codewars/genRaangeofInt.c" "CMakeFiles/Codewars.dir/genRaangeofInt.c.obj" "gcc" "CMakeFiles/Codewars.dir/genRaangeofInt.c.obj.d"
+  "C:/Users/shann/Desktop/C/CLearn/Codewars/isThisaTriangle.c" "CMakeFiles/Codewars.dir/isThisaTriangle.c.obj" "gcc" "CMakeFiles/Codewars.dir/isThisaTriangle.c.obj.d"
+  "C:/Users/shann/Desktop/C/CLearn/Codewars/main.c" "CMakeFiles/Codewars.dir/main.c.obj" "gcc" "CMakeFiles/Codewars.dir/main.c.obj.d"
+  "C:/Users/shann/Desktop/C/CLearn/Codewars/newtester.c" "CMakeFiles/Codewars.dir/newtester.c.obj" "gcc" "CMakeFiles/Codewars.dir/newtester.c.obj.d"
+  "C:/Users/shann/Desktop/C/CLearn/Codewars/removeExclamation.c" "CMakeFiles/Codewars.dir/removeExclamation.c.obj" "gcc" "CMakeFiles/Codewars.dir/removeExclamation.c.obj.d"
   )
 
 # Targets to which this target links which contain Fortran sources.
