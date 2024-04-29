3929c7cc49d365079475432e5d18f011057d3b84
nass179 <135216871+nass179@users.noreply.github.com>
2024-04-29 15:17:11 +0200

    a

---
+++ leetcode/cmake-build-debug/CMakeFiles/leetcode.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/henry/Desktop/CLearn/leetcode/intSum.c" "CMakeFiles/leetcode.dir/intSum.c.obj" "gcc" "CMakeFiles/leetcode.dir/intSum.c.obj.d"
+  "C:/Users/henry/Desktop/CLearn/leetcode/main.c" "CMakeFiles/leetcode.dir/main.c.obj" "gcc" "CMakeFiles/leetcode.dir/main.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ leetcode/cmake-build-debug/CMakeFiles/leetcode.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/leetcode.dir/intSum.c.obj"
+  "CMakeFiles/leetcode.dir/intSum.c.obj.d"
+  "CMakeFiles/leetcode.dir/main.c.obj"
+  "CMakeFiles/leetcode.dir/main.c.obj.d"
+  "leetcode.exe"
+  "leetcode.exe.manifest"
+  "leetcode.pdb"
+  "libleetcode.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/leetcode.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
